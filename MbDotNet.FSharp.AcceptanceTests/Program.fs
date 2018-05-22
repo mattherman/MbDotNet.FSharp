75bd6da6b9c86e940d120328ddf7116c9301e30c
Matthew Herman <mattherman11@gmail.com>
2018-05-22 11:15:16 -0500

    Added tcp test and finished acceptance test framework

--- MbDotNet.FSharp.AcceptanceTests/Program.fs
+++ MbDotNet.FSharp.AcceptanceTests/Program.fs
@@ -3,33 +3,63 @@
 open System.Net
 open MbDotNet.FSharp.Imposters
 open MbDotNet.FSharp.Imposters.Http
+open MbDotNet.FSharp.Imposters.Tcp
 
 let canCreateAndGetHttpImposter =
-    let imposter = httpImposter 4545
+    let port = 4500
+    let imposter = httpImposter port
 
-    imposter |> should
+    imposter |> Http.should
         |> returnStatus HttpStatusCode.OK
         |> onPathAndMethodEqual "/test" Get
         |> ignore
 
-    imposter |> should
+    imposter |> Http.should
         |> returnStatus HttpStatusCode.InternalServerError
         |> onPathAndMethodEqual "/error" Get
         |> ignore
     
     create imposter
 
+    match getHttpImposter port with
+        | Some(_) -> true
+        | None -> false
+
+let canCreateAndGetTcpImposter =
+    let port = 4501
+    let imposter = tcpImposter port
+
+    imposter |> Tcp.should
+        |> returnData "responsedata"
+        |> onDataEquals "requestdata"
+        |> ignore
+
+    create imposter
+
+    match getTcpImposter port with
+        | Some(_) -> true
+        | None -> false
+
 let runTest (testFunc, testFuncName) =
     deleteAll
-    printfn "RUNNING %s" testFuncName
-    testFunc
+
+    let result = testFunc
+    match result with
+        | true -> printfn "PASSED %s" testFuncName
+        | false -> printfn "FAILED %s" testFuncName
+
+    result
+
 
 let tests = [
-    (canCreateAndGetHttpImposter, "canCreateAndGetHttpImposter")
+    (canCreateAndGetHttpImposter, "canCreateAndGetHttpImposter");
+    (canCreateAndGetTcpImposter, "canCreateAndGetTcpImposter")
 ]
 
 [<EntryPoint>]
 let main argv =
-    List.map runTest tests |> ignore
+    List.map runTest tests 
+        |> List.countBy (fun result -> if result then "PASS" else "FAIL")
+        |> printfn "%A"
 
     0 // return an integer exit code
